5297f98130de75fb97709ff9a924de99a26115ac
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-09-09 10:15:59 -0500

    add removeElement

---
+++ 27-removeElement.js
@@ -0,0 +1,9 @@
+var removeElement = function(nums, val) {
+    let len = nums.length
+    for (let i = 0; i < len; i++) {
+      if (nums[i] === val) {
+        nums[i--] = nums[--len]
+      }
+    }
+    return len
+  };
