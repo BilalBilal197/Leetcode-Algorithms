d6a5af65e55785183a845e44c845d28356c81268
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-10-20 17:32:35 -0500

    add desc for pairboard

--- pairboard10-20.js
+++ pairboard10-20.js
@@ -1,3 +1,33 @@
+/*
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// # declare variable product  to zero
+
+// # make combination of all subarrays
+
+// # get products of element of subarrays
+
+// # compare products
+
+// # return bigger product
+
+
 var maxProduct = function(nums) {
 
     let bigger = 0
