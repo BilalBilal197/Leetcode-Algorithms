0c9d6c7fe2fd05736139e8ab27af9acb72ccdeb6
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-11-25 15:43:58 -0600

    add leetcode 112

---
+++ 112-partSum.js
@@ -0,0 +1,11 @@
+var hasPathSum = function(root, sum) {
+    if(root === null){
+        return false;
+    }
+    
+    if(root.val === sum && root.left === null && root.right === null){
+        return true;
+    }
+    
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
