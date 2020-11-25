aa36463b8f37769fe33925cec31fc49c39cb02cb
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-11-25 15:44:24 -0600

    update partSum

--- 112-partSum.js
+++ 112-partSum.js
@@ -9,3 +9,21 @@ var hasPathSum = function(root, sum) {
     
     return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
 };
+
+
+
+
+var hasPathSum = function(root, sum) {
+    if (root === null) {
+        return false;
+    }
+    
+    var left = root.left;
+    var right = root.right;
+    
+    if (left === null && right === null) {
+        return root.val === sum;
+    }
+    
+    return hasPathSum(left, sum - root.val) || hasPathSum(right, sum - root.val);
+};
